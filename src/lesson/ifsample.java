734a33d411f6cbf938bf521606031058f84d3ed6
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-22 15:50:51 +0900

    5か8ならあたり

--- src/lesson/ifsample.java
+++ src/lesson/ifsample.java
@@ -3,10 +3,10 @@ package lesson;
 public class ifsample {
 
 	public static void main(String[] args) {
-		//入力した数が5以上10以下ならあたり　それ以外ははずれ
+		//入力した数が5または8ならあたり　それ以外ははずれ
 		System.out.println("数を入力してください：");
 		int num = new java.util.Scanner(System.in).nextInt();
-		if(num >= 5 && num <= 10) {
+		if(num == 5 || num == 8) {
 			System.out.println("あたり");
 		}else {
 			System.out.println("はずれ");
