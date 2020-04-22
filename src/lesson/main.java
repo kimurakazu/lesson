975f1ea805b4071db397a42eb7e5a6a5f7d4c40e
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-22 13:28:36 +0900

    プッシュテスト

--- src/lesson/main.java
+++ src/lesson/main.java
@@ -3,6 +3,9 @@ package lesson;
 public class main {
 
 	public static void main(String[] args) {
+		int tuki =4,hi = 22;
+		System.out.println("今日は" + tuki + "月" + hi + "日");
+		
 		double p = 3.14;
 		System.out.println("円周率は" + p + "です。");
 	
