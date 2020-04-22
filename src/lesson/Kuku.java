73c35fd8c35f262fd0358d40b1700a6846d910b1
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-22 16:41:44 +0900

    九九

--- src/lesson/Kuku.java
+++ src/lesson/Kuku.java
@@ -5,12 +5,14 @@ public class Kuku {
 	public static void main(String[] args) {
 		int num1 =  new java.util.Random().nextInt(9) + 1;
 		int num2 =  new java.util.Random().nextInt(9) + 1;
+		int num3 = num1 * num2;
 		System.out.println(num1 + "*" + num2 + "=");
 		int answer = new java.util.Scanner(System.in).nextInt();
-		if( (num1 * num2) == answer ) {
+		
+		if( num3 == answer ) {
 			System.out.println("正解");
 		}else {
-			System.out.println("間違い。正解は" + (num1 * num2));
+			System.out.println("間違い。正解は" + num3);
 		}
 
 	}
