d68923e6b1c0b2224c22b577cd73541e5e6f9033
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-22 14:17:55 +0900

    平均点の問題

--- src/lesson/main.java
+++ src/lesson/main.java
@@ -8,8 +8,28 @@ public class main {
 		
 		double p = 3.14;
 		System.out.println("円周率は" + p + "です。");
-	
+		 
+		int a = 20 ,b;
+		b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("今日は\"四月\"です");
+		System.out.println("\\100\n");
 
+		System.out.println("C:に\"フォルダ\"を作成\n");
+		
+		int num = 10,amari= num % 3;
+		System.out.println(num + "/3の余りは" + amari + "\n");
+		
+		double d = 3;
+		int n = (int)3.2;
+		String s= "ベスト" + 3;
+		
+		int kokugo = 70;
+		int sansu = 75;
+		double avg = (kokugo + sansu) / 2.0;
+		System.out.println("平均点は" + avg);
+		
 	}
 
 }
