f8001f3639bcedec0e7765d51e233c3977b1f9e2
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-22 15:36:13 +0900

    10ならあたり

---
+++ src/lesson/ifsample.java
@@ -0,0 +1,17 @@
+package lesson;
+
+public class ifsample {
+
+	public static void main(String[] args) {
+		//入力した数が10以上ならあたり　それ以外ははずれ
+		System.out.println("数を入力してください：");
+		int num = new java.util.Scanner(System.in).nextInt();
+		if(num >= 10) {
+			System.out.println("あたり");
+		}else {
+			System.out.println("はずれ");
+		}
+
+	}
+
+}
