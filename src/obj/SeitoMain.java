11a93c4aa8079cca86f4119563e7534d8ecda134
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-04-28 16:44:39 +0900

    中学生 修正版

--- src/obj/SeitoMain.java
+++ src/obj/SeitoMain.java
@@ -5,15 +5,19 @@ public class SeitoMain {
 
 	public static void main(String[] args) {
 		ArrayList<Chugakusei> cg = new ArrayList<Chugakusei>();
-		
+
 		cg.add(new Chugakusei("田中",50,40,70,80));
 		cg.add(new Chugakusei("湊",70,60,40,50));
 		cg.add(new Chugakusei("朝香",80,60,90,70));
-		
+
+		int sum=0;
 		for(Chugakusei c:cg) {
 			c.goukei();
+			sum += c.goukei();
 			c.goukeiShow();
+
 		}
+		System.out.println("全員の合計点は" + sum);
 
 	}
 
