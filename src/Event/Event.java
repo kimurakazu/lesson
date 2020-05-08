630b866d64c49c7b38b5cae27b3d16e2851ee7f0
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-05-08 16:32:26 +0900

    イベント

--- src/Event/Event.java
+++ src/Event/Event.java
@@ -13,6 +13,8 @@ public class Event {
 
 	public Event(String name) {
 		this.name = name;
+
+		//日付の設定 ここを消すと今日の日付になる
 		Calendar c = Calendar.getInstance();
 		c.set(2020,5-1,8);
 		hi = c.getTime();
@@ -23,9 +25,14 @@ public class Event {
 	}
 
 	public void show() {
+		//イベントの名前表示
 		System.out.println(this.name);
+
+		//日付の表示
 		SimpleDateFormat f = new SimpleDateFormat("yyyy年MM月dd日");
 		System.out.println(f.format(hi));
+
+		//参加者の表示
 		for(Sankasha s : sanka) {
 			System.out.println(s.getName() + " " + s.getAge());
 		}
--- src/Event/EventMain.java
+++ src/Event/EventMain.java
@@ -3,12 +3,16 @@ package Event;
 public class EventMain {
 
 	public static void main(String[] args) {
-		
+
+		//イベントの作成
 		Event e = new Event("春の人狼大会");
+
+		//参加者の登録
 		e.add(new Sankasha("田中",30));
 		e.add(new Sankasha("山田",25));
 		e.add(new Sankasha("山本",35));
-		
+
+		//詳細表示
 		e.show();
 
 	}
